7bd7af623f3ab0778ce7e79960038299659a35f7
pavankumar <pavankumarkosaraju@gmail.com>
2024-09-09 21:56:19 +0530

    created Layouts using nextFont and metaData

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,31 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
 
 import "./global.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "devOverFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions Get help with your code. Explore and share the best programming resources.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -15,15 +38,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           {children}
         </body>
       </html>
